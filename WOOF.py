c5e6f540cdaba8ab0a5ee8a545b83f26c04b0395
BrianDai <2687126832@qq.com>
2022-10-15 13:47:00 -0400

    Initial commit

---
+++ WOOF.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import re
+
+class WOOF(object):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        
+    def get_catergories(self, col):
+        return set(self.dataframe.col)
+    
+    def 
+    
+    def woof(self, brand, bio = None, ads_type = None):
+        pass
+        # bio:[list:str]
+        #if bio:
+            
